37ba4016b6acc4126ade576b46c277ae514687d4
xnng <xnng77@gmail.com>
2024-12-28 08:49:48 +0800

    update

--- scripts/generate-icons.js
+++ scripts/generate-icons.js
@@ -16,24 +16,6 @@ async function generateIcons() {
     }
   } catch (error) {
     console.error('Error generating icons:', error);
-const sharp = require('sharp');
-const path = require('path');
-
-const sizes = [16, 48, 128];
-const inputFile = path.join(__dirname, '../public/assets/icon.svg');
-
-async function generateIcons() {
-  try {
-    for (const size of sizes) {
-      const outputFile = path.join(__dirname, `../public/assets/icon${size}.png`);
-      await sharp(inputFile)
-        .resize(size, size)
-        .png()
-        .toFile(outputFile);
-      console.log(`Generated icon${size}.png`);
-    }
-  } catch (error) {
-    console.error('Error generating icons:', error);
   }
 }
 
